512fdf8bfcf46b8744779aabc681a0957572bae6
Thomas MacLean <thomas@weareantenna.be>
2025-05-24 20:13:24 +0200

    free

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const imageShortcode = require("./utils/image");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("public"); // Makes "public/" available in _site/
+
   eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
   eleventyConfig.addLiquidShortcode("image", imageShortcode);
   return {
